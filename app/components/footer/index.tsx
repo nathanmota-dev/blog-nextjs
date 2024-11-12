e07ccdf1c0cab0007c2f954eecc5344b0a6313e3
Nathan <nathansmota@gmail.com>
2024-11-11 23:22:43 -0300

    :lipstick: feat: style page

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -7,21 +7,24 @@ export default function Footer() {
             <ul className="flex items-center justify-center gap-3">
                 <li>
                     <a href="mailto:nathansmota@gmail.com">
-                        <MdOutlineEmail size={28} />
+                        <MdOutlineEmail className="text-black dark:text-white hover:text-black/50
+                        dark:hover:text-zinc-200 transition-colors" size={28} />
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:nathansmota@gmail.com">
-                        <FaLinkedinIn size={28} />
+                    <a href="https://www.linkedin.com/in/nathansmota/">
+                        <FaLinkedinIn className="text-black dark:text-white hover:text-black/50
+                        dark:hover:text-zinc-200 transition-colors" size={28} />
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:nathansmota@gmail.com">
-                        <FaGithub size={28} />
+                    <a href="https://github.com/nathanmota-dev">
+                        <FaGithub className="text-black dark:text-white hover:text-black/50
+                        dark:hover:text-zinc-200 transition-colors" size={28} />
                     </a>
                 </li>
             </ul>
-            <h2 className="text-sm text-slate-700 dark:text-muted-foreground text-justify mt-2">Nathan Mota</h2>
+            <h2 className="text-sm text-black dark:text-muted-foreground text-justify mt-2">Nathan Mota</h2>
         </footer>
     )
 }
